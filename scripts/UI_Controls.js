5f0299553b1a7a5385eacd00f8c60fd3b589e129
NiallPhillips <niallphillips0215@gmail.com>
2017-02-15 15:56:46 +0000

    Tidying & commenting

--- scripts/UI_Controls.js
+++ scripts/UI_Controls.js
@@ -1,3 +1,4 @@
+//Control object holds all variables the user will have access to
 control = new function() {
 	this.isPlaying = false;
 	this.playPause = function() {
@@ -25,6 +26,7 @@ control = new function() {
 	this.Reverb = 0.00;
 };
 
+//Add eventListener for keyboard shortcuts
 document.addEventListener('keydown',(event) => {
 	if(event.key == ' ') {
 		control.isPlaying = !control.isPlaying;
@@ -33,14 +35,15 @@ document.addEventListener('keydown',(event) => {
 
 addControlGui(control);
 
+//Creates new GUI object and adds elements from the control object to it
 function addControlGui(controlObject) {
 	var gui = new dat.GUI();
-	gui.closed=true;
+	gui.closed=true; //Gui is closed on start
 	gui.add(controlObject, 'playPause').name("Play/Pause");
 	gui.add(controlObject, 'makeWholeCube').name("Add Whole Note");
 	gui.add(controlObject, 'makeHalfCube').name("Add Half Note");
 	gui.add(controlObject, 'makeQuarterCube').name("Add Quarter Note");
-	gui.add(controlObject, 'BPM', 0, 240).name("Tempo");
+	gui.add(controlObject, 'BPM', 0, 300).name("Tempo");
 	gui.add(controlObject, 'Delay', 0, 10).name("Delay");
 	gui.add(controlObject, 'Reverb', 0, 10).name("Reverb");
 	gui.add(controlObject, 'Wave', ['sine','triangle','sawtooth','square']);
--- scripts/oscillator.js
+++ scripts/oscillator.js
@@ -1,83 +1,95 @@
-Physijs.scripts.worker = 'physijs_worker.js';
-Physijs.scripts.ammo = 'ammo.js';
+// Define script references required for physi.js
+Physijs.scripts.worker = '/scripts/physijs_worker.js';
+Physijs.scripts.ammo = '/scripts/ammo.js';
 
+//Create the 3D scene, camera and renderer
 var scene = new Physijs.Scene({ reportsize: 50, fixedTimeStep: 1 / 60 });
-scene.setGravity(new THREE.Vector3(0, -50, 0));
-
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 var renderer = new THREE.WebGLRenderer({antialias: true});
 
+// Create new instance of OrbitControls for mouse movement
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableZoom = true;
 
+// Create a cube geometry from which all the scenes cubes are cloned
 var cubeGeom = new THREE.CubeGeometry(10, 10, 10);
-var maxAnisotropy = renderer.getMaxAnisotropy();
-var noteImage = new THREE.TextureLoader().load("quaver.jpg");
-noteImage.anisotropy = maxAnisotropy;
 
-var cubes = [];
-var cubeColliders = [];
-var box3Colliders = [];
-var hitFlags = [];
+//Load the image to be used to texture the cubes
+var noteImage = new THREE.TextureLoader().load("images/quaver.jpg");
+
+
+var cubes = [];			//Holds all of the scenes cubes
+var cubeColliders = [];	//Holds all of the cubes boxHelper objects
+var box3Colliders = [];	//Holds all of the boxHelper collision boxes
+var hitFlags = [];		//Flags used to ensure collision is detected only once every bar pass
 var bar;
 var barBoundingBox;
 var barBox;
 var audioctx = new AudioContext();
 
 var clock;
-var time;
 var timeDelta;
 
+
 function initScene() {
-    "use strict";
+    "use strict"; //Prevents accidental creation of global variables
     
+	//Initialize the scene and add the renderer to the window
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
-	
 	window.addEventListener('resize', onWindowResize, false);
 	
-	clock = new THREE.Clock;
+	scene.setGravity(new THREE.Vector3(0, -50, 0)); //Set the gravity for the physics
 	
-	var light = new THREE.AmbientLight(0x404040, 4); // soft white light
+	clock = new THREE.Clock; //Create a clock for handling animation speeds
+	
+	// Add lights to the scene
+	var light = new THREE.AmbientLight(0x404040, 4);
 	scene.add(light);
-	
 	var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 	directionalLight.position.set(0, 1, 0.5);
+	directionalLight.target.position.set(0, 0, 0);
 	scene.add(directionalLight);
 	
+	//Set anisotropic filtering to maximum for the cubes texture image
+	var maxAnisotropy = renderer.getMaxAnisotropy();
+	noteImage.anisotropy = maxAnisotropy;
+	
+	//Create the physical ground object
 	var groundGeometry = new THREE.BoxGeometry(100, 1, 100);
 	var groundMaterial = new THREE.MeshBasicMaterial({color: 0xff0000,wireframe:true});
 	var ground = new Physijs.BoxMesh(groundGeometry, groundMaterial, 0);
 	
-	var bargeometry = new THREE.BoxGeometry(6, 100, 0.1);
+	//Create the time 'bar' plane object, set its location and size
+	var bargeometry = new THREE.PlaneGeometry(6, 100);
 	var barmaterial = new THREE.MeshBasicMaterial({color: 0xffff00,wireframe:true});
 	bar = new THREE.Mesh(bargeometry, barmaterial);
 	barBox = new THREE.Box3();
 	barBox.setFromObject(bar);
 	bar.position.set(50, 3.5, 0);
+	bar.rotation.x = Math.PI / 2;
+	bar.rotation.y = Math.PI / 2;
 	
+	//Create the collision box for the bar object
 	barBoundingBox = new THREE.BoxHelper(bar, 0x00ff00);
 	barBoundingBox.visible = true;
 	scene.add(bar);
 	scene.add(barBoundingBox);
 	
-	bar.rotation.x = Math.PI / 2;
-	bar.rotation.y = Math.PI / 2;
-	
 	scene.add(ground);
 	
-	camera.position.z = -80;
-	camera.position.y = 50;
+	camera.position.z = -100;
+	camera.position.y = 80;
 	
-	updateCubeCounter();
+	updateCubeCounter(); //Initialize cube counter
 	
 	var gridHelper = new THREE.GridHelper( 50, 8 );
-	// gridHelper.position = (0,5,0);
 	scene.add( gridHelper );
 	
 	render();
 }
 
+//Helper function to return a random +1 or -1
 function PlusOrMinus() {
     "use strict";
     var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
@@ -85,13 +97,16 @@ function PlusOrMinus() {
 }
 
 function render() {
-    "use strict";
+    "use strict"; 
 	requestAnimationFrame(render);
 	////////////////////////////////////
-	timeDelta = clock.getDelta();
-	controls.update();
-	scene.simulate();
+	timeDelta = clock.getDelta(); //Update clock used for controlling movement speed every render pass
+	controls.update(); //Update camera with 'OrbitControls' input from the user
+	scene.simulate(); //Render phsyics
 	
+	
+	//Loop handles the movement and playback of the 'bar' object
+	//It updates the bars position, hitbox, movement speed if the application 'isPlaying'
 	if(!control.isPlaying) {
 		bar.position.set(50, 3.5, 0);
 		barBoundingBox.update(bar);
@@ -100,22 +115,23 @@ function render() {
 		if (bar.position.x < -49.9) {
 			bar.position.set(50, 3.5, 0);
 			for(var i = 0; i < hitFlags.length; i++) {
-				hitFlags[i] = 0;
+				hitFlags[i] = 0; //Resets all 'hitFlags' when 'bar' is reset
 			}
 		}
 		else {
-			bar.position.x -= (control.BPM/60)*timeDelta*25; // moves 100 units per second, in this case, 4 beats per second
+			bar.position.x -= (control.BPM/60)*timeDelta*25; //Sets movement speed based on 'control.BPM'
 		}
 		barBoundingBox.update(bar);
 		barBox.setFromObject(bar);
 	}
 	
-	
+	//Loop through all cubes and update there collision boxes, collision detection
+	//Also deletes cubes from the scene if it has fallen off the ground
 	for (var i = 0;i<cubes.length;i++) {
 		
 		cubeColliders[i].update(cubes[i].mesh);
 		box3Colliders[i].setFromObject(cubeColliders[i]);
-		cubes[i].updateNoteLength();
+		
 		
 		if(box3Colliders[i].intersectsBox(barBox)&&hitFlags[i]==0&&control.isPlaying) {
 			hitFlags[i] = 1;
@@ -138,6 +154,8 @@ function render() {
 	renderer.render( scene, camera );
 }
 
+//onWindowResize handles the resizing of the browser window
+//It updates the camera aspect ratio and renderer size
 function onWindowResize() {
 	
 	windowHalfX = window.innerWidth / 2;
@@ -150,27 +168,36 @@ function onWindowResize() {
 	
 }
 
+//Entry point for the application
+//Function called on window load
 function startApp() {
 	
 	initScene();
 	
 }
 
+//newCube() creates a new instance of Cube, BoxHelper and Box3
+//These instances are appended to the corresponding array and added to the scene
 function newCube(noteValue) {
 	if(cubes.length < 8) {
 		cubes[cubes.length] = new Cube(noteValue);
 		cubeColliders[cubeColliders.length] = new THREE.BoxHelper(cubes[cubes.length-1].mesh,0x0000ff);
 		box3Colliders[box3Colliders.length] = new THREE.Box3();
+		
 		box3Colliders[box3Colliders.length-1].setFromObject(cubeColliders[cubeColliders.length-1]);
+		
 		hitFlags[hitFlags.length] = 0;
 		
-		cubeColliders[cubeColliders.length-1].visible = true;
+		cubeColliders[cubeColliders.length-1].visible = true; //Set cubes bounding box visible
 		
+		//Give the cube a random spawn point in a 45X45 square 20 units above the ground mesh
 		cubes[cubes.length-1].mesh.position.set(
 		Math.random()*-45*PlusOrMinus(),
 		20,
 		Math.random()*-45*PlusOrMinus()
 		);
+		
+		//Give the cube a random rotation on all axes
 		cubes[cubes.length-1].mesh.rotation.set(
 		Math.random()*Math.PI*2,
 		Math.random()*Math.PI*2,
@@ -178,24 +205,33 @@ function newCube(noteValue) {
 		);
 		scene.add(cubes[cubes.length-1].mesh);
 		scene.add(cubeColliders[cubeColliders.length-1]);
+		
+		cubes[cubes.length-1].updateNoteLength(); //Sets the length of the cubes note based on its noteValue
+		
 		updateCubeCounter();
 		} else {
 		alert("You Appear to have run out of cubes!")
 	}
 }
 
+//Cube object holds all information about a cube (geometry, material, mesh, note information etc.)
+//All the functions for updating and playing audio are defined here
 function Cube(scalar) {
 	this.geometry = cubeGeom.clone();
 	this.material = new Physijs.createMaterial(new THREE.MeshStandardMaterial({map:noteImage}),0.4,1);
 	this.mesh = new Physijs.BoxMesh( this.geometry, this.material,3 );
 	this.mesh.scale.set(scalar,scalar,scalar);
-	// this.note = NOTE_C[4];
-	this.noteLength = 1*scalar;
+	this.noteLength;
 	var oscillator, gainNode;
 	
+	//Set the note length based on control.BPM
 	this.updateNoteLength = function(){
 		this.noteLength = 1/(control.BPM/60)*scalar
 	};
+	
+	//this.updateNote is repeatedly called in the render loop to update a cubes note value.
+	//The floor geometry is divided into steps of 8 values and the cubes position on that grid
+	//determines its note frequency
 	this.updateNote = function(){
 		var zpos = this.mesh.position.z;
 		switch(true){
@@ -233,35 +269,33 @@ function Cube(scalar) {
 			break;
 		}
 	};
+	
+	//Function to snap cube position to timing grid
 	this.quantize = function(){
 		
 	};
+	
+	//this.playNote creates a new oscillator, sets its note, gain and wave type.
+	//The note is then played for a duration of 'noteLength'
 	this.playNote = function(){
 		this.updateNote();
+		//A new oscillator must be created everytime a note is played
 		oscillator = audioctx.createOscillator();
 		oscillator.frequency.value = this.note;
 		gainNode = audioctx.createGain();
-		gainNode.gain.value = 0.3; // Sounds less harsh when gain is turned down a little
+		gainNode.gain.value = 0.3; // Reduced gain to prevent clipping
 		oscillator.type = control.Wave;
 		oscillator.connect(gainNode);
 		gainNode.connect(audioctx.destination);
 		oscillator.start(0);
 		gainNode.gain.setTargetAtTime(0, audioctx.currentTime+this.noteLength, 0.01);
-		// oscillator.stop(audioctx.currentTime+this.noteLength);
 	};
 }
 
+//This function updates the 'cubeCounter' DOM element to display the current
+//number of cubes remaining.
 function updateCubeCounter() {
 	if(cubes.length==8) {
-		document.getElementById('cubeCounter').innerHTML = ("Cubes left: "+ (8-cubes.length)+" :(");
-	}else {document.getElementById('cubeCounter').innerHTML = ("Cubes left: "+ (8-cubes.length));}
-}
-
-function TimeBar() {
-	this.plane = 0;
-	this.tempo = 0;
-	
-	this.updateTempo = function(){};
-	this.start = function(){};
-	this.stop = function(){};
+		document.getElementById('cubeCounter').innerHTML = ("Cubes remaining: "+ (8-cubes.length)+"<br> :(");
+	}else {document.getElementById('cubeCounter').innerHTML = ("Cubes Remaining: "+ (8-cubes.length));}
 }
